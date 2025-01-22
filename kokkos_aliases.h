d6063d0898777c7d00fc88cf0ecda612a1d3f8a3
Remi Bourgeois <Remi.Bourgeois@cea.fr>
2025-01-22 20:21:43 +0100

    update aliases

--- kokkos_aliases.h
+++ kokkos_aliases.h
@@ -4,9 +4,17 @@
 // Kokkos aliases
 template <typename _TYPE_>
 using View = Kokkos::View<_TYPE_ *>;
-template <typename _TYPE_>
-using HostView = Kokkos::View<_TYPE_ *, Kokkos::DefaultHostExecutionSpace>;
+
 using Device = Kokkos::DefaultExecutionSpace;
+using Host = Kokkos::DefaultHostExecutionSpace;
+using HostPinned = Kokkos::SharedHostPinnedSpace;
+
+template <typename _TYPE_>
+using HostView = Kokkos::View<_TYPE_ *, Host>;
+
+template <typename _TYPE_>
+using HostPinnedView = Kokkos::View<_TYPE_ *, HostPinned>;
+
 using policy_t = Kokkos::RangePolicy<Device>;
 using team_policy_t = Kokkos::TeamPolicy<Device>;
 using member_type = team_policy_t::member_type;
@@ -22,5 +30,7 @@ template <typename _TYPE_>
 using Subview = Kokkos::Subview<View<_TYPE_>>;
 template <typename _TYPE_>
 using HostSubview = Kokkos::Subview<HostView<_TYPE_>>;
+template <typename _TYPE_>
+using HostPinnedViewSubview = Kokkos::Subview<HostPinnedView<_TYPE_>>;
 
 #endif
